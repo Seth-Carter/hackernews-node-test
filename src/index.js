146bf2c344e12ab797e472a9ed96e4799f7174eb
Seth Carter <carter.seth.m@gmail.com>
2020-09-09 08:10:42 +0100

    Separated graphql schema into its own file. Added a simple mutation resolver.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQl'
+}]
 
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
+let idCount = links.length
+
 const resolvers = {
   Query: {
-    info: () => null
+    info: () => 'This is the API of a Hackernews Clone',
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 
