8813f84b7c7d18a6020553f3ac354b980e487c3d
Seth Carter <carter.seth.m@gmail.com>
2020-09-10 11:34:21 +0100

    Added subscription to graphql schema

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+const { PubSub } = require('graphql-yoga')
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQl'
-}]
-
-let idCount = links.length
+const pubsub = new PubSub()
+const prisma = new PrismaClient()
 
 const resolvers = {
-  Query: {
-    info: () => 'This is the API of a Hackernews Clone',
-    feed: () => links,
-  },
-  Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
-  }
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma,
+      pubsub,
+    }
+  },
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
